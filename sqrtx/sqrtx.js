ebb67c3c72e6523146d84b852cac792087418cd6
Matt Salmons <69879950+mattsalmons@users.noreply.github.com>
2021-07-06 10:56:38 -0600

    Time: 88 ms (88.98%), Space: 40.5 MB (10.51%) - LeetHub

--- sqrtx/sqrtx.js
+++ sqrtx/sqrtx.js
@@ -3,13 +3,6 @@
  * @return {number}
  */
 
-// input: positive integer
-// output: truncated square root of input
-// constraints: no built in exponent funcs or operators (**)
-// edge cases: no x return null
-// approach:
-//  - handle edge case
-//  - 
 var mySqrt = function(x) {
-    return Math.trunc(Math.sqrt(x))
+    return Math.floor(Math.sqrt(x))
 };
