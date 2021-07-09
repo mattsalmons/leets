af1b0f470da4031f328831f14106e48e2765dfb9
Matt Salmons <69879950+mattsalmons@users.noreply.github.com>
2021-07-09 10:14:36 -0600

    Time: 72 ms (85.82%), Space: 40 MB (8.24%) - LeetHub

---
+++ merge-sorted-array/merge-sorted-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+
+
+var merge = function(nums1, m, nums2, n) {
+    nums2.forEach(num =>{
+        nums1.unshift(num)
+        nums1.pop()
+    })
+    nums1.sort((a, b) => a - b)
+};
