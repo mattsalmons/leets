9154d301d0fd51dc28c02ff96f1b8143377deb95
Matt Salmons <69879950+mattsalmons@users.noreply.github.com>
2021-08-02 10:20:53 -0600

    Time: 88 ms (97.91%), Space: 44.5 MB (58.18%) - LeetHub

---
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+// input:
+//  - string of letters
+// output:
+//  - integer representing the length of the longest substring contained within the input that has no repeated chars
+// edge cases:
+//  - empty string return 0
+// approach:
+//  - handle edge case
+//  - init current, next pointers
+//  - init currentLength
+//  - init longest
+//  - while current is less than next and next is less than length of input
+//      - if s at current index is not the same as at next
+//          - increment currentLength and next
+//          - update longest to max of longest and currentlength
+//      - if s at current is same as at next
+//          - set currentLength to one
+//          - increment current and next
+//          - update longest
+//  return longest
+var lengthOfLongestSubstring = function(s) {
+  let longestStr = '';
+  let currentStr = '';
+
+  for(let i = 0; i < s.length; i++) {
+    let letter = s[i];
+    let index = currentStr.indexOf(letter);
+
+    if(index > -1) {
+      if(currentStr.length > longestStr.length) longestStr = currentStr;
+      currentStr = currentStr.slice(index + 1) + letter;
+    }
+    else {
+      currentStr += letter;
+    }
+  }
+  if(currentStr.length > longestStr.length) longestStr = currentStr;
+  return longestStr.length;
+};
