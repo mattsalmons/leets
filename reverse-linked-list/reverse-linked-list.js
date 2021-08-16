97fa47bec3d42fd11928b43bcce11b6317d4dbb1
Matt Salmons <69879950+mattsalmons@users.noreply.github.com>
2021-08-16 09:18:57 -0600

    Time: 72 ms (91.59%), Space: 40.7 MB (45.73%) - LeetHub

---
+++ reverse-linked-list/reverse-linked-list.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    var node = head;
+    var previous = null;
+
+    while (node) {
+        // save next or you lose it!!!
+        var save = node.next;
+        // reverse pointer
+        node.next = previous;
+        // increment previous to current node
+        previous = node;
+        // increment node to next node or null at end of list
+        node = save;
+    }
+  return previous;   // Change the list head !!!
+};
