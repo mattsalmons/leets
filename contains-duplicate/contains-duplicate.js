d71cf1c94d0b930de8ad5dfb808a345ab994c21b
Matt Salmons <69879950+mattsalmons@users.noreply.github.com>
2021-08-17 21:09:55 -0600

    Time: 72 ms (97.38%), Space: 45.4 MB (44.78%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let set = [... new Set(nums)]
+    
+    if (set.length !== nums.length) return true;
+    
+    return false;
+};
