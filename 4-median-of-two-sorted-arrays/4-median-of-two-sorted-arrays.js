54ca974c8b6b8633292c0cb0687b32cb387eb91e
Matt Salmons <69879950+mattsalmons@users.noreply.github.com>
2022-01-16 10:17:31 -0700

    Time: 205 ms (27.77%), Space: 44.5 MB (70.54%) - LeetHub

---
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    let merged = nums1.concat(nums2);
+    let sorted = merged.sort((a, b) => a - b);
+    
+    var half = Math.floor(sorted.length / 2);
+  
+    if (sorted.length % 2) return sorted[half];
+  
+    return (sorted[half - 1] + sorted[half]) / 2;
+};
