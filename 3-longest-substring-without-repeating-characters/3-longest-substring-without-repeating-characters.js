7bc407eaba8cb6b3e5fa1d7b534e16f6a818593c
Matt Salmons <69879950+mattsalmons@users.noreply.github.com>
2022-01-16 09:32:59 -0700

    Time: 84 ms (99.25%), Space: 44.1 MB (61.90%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -24,24 +24,45 @@
 //          - update longest
 //  return longest
 
-  
+//   let longestStr = '';
+//   let currentStr = '';
+
+//   for(let i = 0; i < s.length; i++) {
+//     let letter = s[i];
+//     let index = currentStr.indexOf(letter);
+
+//     if(index > -1) {
+//       if(currentStr.length > longestStr.length) longestStr = currentStr;
+//       currentStr = currentStr.slice(index + 1) + letter;
+//     }
+//     else {
+//       currentStr += letter;
+//     }
+//   }
+//   if(currentStr.length > longestStr.length) longestStr = currentStr;
+//   return longestStr.length;
 
 var lengthOfLongestSubstring = function(s) {
-  let longest = '';
-  let current = '';
-
-  for(let i = 0; i < s.length; i++) {
-    let letter = s[i];
-    let index = current.indexOf(letter);
-
-    if(index > -1) {
-      if(current.length > longest.length) longest = current;
-      current = current.slice(index + 1) + letter;
+    let current = '';
+    let longest = '';
+    
+    for (let i = 0; i < s.length; i++) {
+        let letter = s[i];
+        let index = current.indexOf(letter);
+        
+        if (index > -1) {
+            if (current.length > longest.length) {
+                longest = current;
+            }
+            current = current.slice(index + 1) + letter;
+        } else {
+            current += letter;
+        }
     }
-    else {
-      current += letter;
+    
+    if (current.length > longest.length) {
+        longest = current;
     }
-  }
-  if(current.length > longest.length) longest = current;
-  return longest.length;
+    
+    return longest.length;
 };
