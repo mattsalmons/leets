9a8748385e05c61b9efe606d4cad92849b577e32
Matt Salmons <69879950+mattsalmons@users.noreply.github.com>
2022-01-18 07:24:09 -0700

    Time: 132 ms (56.95%), Space: 45.5 MB (22.28%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -14,33 +14,32 @@
 //  iterate over input
 //      create alias for current letter
 //      init index, assign as location of current letter in current string
-//      if current letter is found in current
+//      if current letter is not in current string
+//          add current letter to current string
+//              otherwise
 //          if current string length is greater than longest string length
 //              reassign longest string to current string
-//          reassign current string to equal a slice of previous current string from index                 after found letter, add current letter
-//              otherwise
-//          add current letter to current string
+//              reassign current string to equal a slice of previous current string starting                   from index after found letter, concatinate current letter
 //      if current string length is greater than longest string length
 //          reassign longest string to current string
 //      return longest string length
 
 var lengthOfLongestSubstring = function(s) {
-    let current = '';
-    let longest = '';
+    if (!s.length) return 0;
+    
+    let [current, longest] = ['', ''];
     
     for (let i = 0; i < s.length; i++) {
         let letter = s[i];
         let index = current.indexOf(letter);
         
-        if (index === -1) {
-            current += letter;
-        } else {
+        if (index === -1) current += letter;
+        else {
             if (current.length > longest.length) longest = current;
             current = current.slice(index + 1) + letter;
         }
+        if (current.length > longest.length) longest = current;
     }
-    
-    if (current.length > longest.length) longest = current;
-    
+    console.log(longest);
     return longest.length;
-};
+}
