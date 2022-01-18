ff5c5b06746eda6e2ae9c99635edc09062d4485d
Matt Salmons <69879950+mattsalmons@users.noreply.github.com>
2022-01-17 17:09:50 -0700

    Time: 187 ms (31.18%), Space: 44.1 MB (71.93%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.js
+++ 2-add-two-numbers/2-add-two-numbers.js
@@ -17,8 +17,43 @@
 //  interate over lists in reverse
 //      collect digits and convert to numbers
 var addTwoNumbers = function(l1, l2) {
-    let list = new ListNode(0);
+//     let list = new ListNode(0);
+//     let head = list;
+//     let sum = 0;
+//     let carry = 0;
+    
+//     while (l1 || l2 || sum) {
+//         if (l1) {
+//             sum += l1.val;
+//             l1 = l1.next;
+//         }
+        
+//         if (l2) {
+//             sum += l2.val;
+//             l2 = l2.next;
+//         }
+        
+//         if (sum >= 10) {
+//             carry = 1;
+//             sum -= 10;
+//         }
+        
+//         head.next = new ListNode(sum);
+//         head = head.next;
+        
+//         sum = carry;
+//         carry = 0;
+        
+//         console.log(sum)
+//         console.log(list)
+//     }
+   
+    
+//     return list.next;
+    
+    let list = new ListNode();
     let head = list;
+    
     let sum = 0;
     let carry = 0;
     
@@ -43,11 +78,7 @@ var addTwoNumbers = function(l1, l2) {
         
         sum = carry;
         carry = 0;
-        
-        console.log(sum)
-        console.log(list)
     }
-   
     
-    return list.next;
+    return list.next
 };
