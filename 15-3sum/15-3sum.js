3dae3e819e25f0771b574cb5aaca0fcbce7aec40
Matt Salmons <69879950+mattsalmons@users.noreply.github.com>
2022-01-20 13:09:11 -0700

    Time: 136 ms (93.24%), Space: 49.3 MB (44.70%) - LeetHub

---
+++ 15-3sum/15-3sum.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+
+var threeSum = function(nums) {
+    let triplets = [];
+    
+    const sortedArr = nums.sort((a, b) => a - b);
+    
+    for (i = 0; i < sortedArr.length - 2; i++) {
+        
+      if (sortedArr[i] > 0) {
+        break;
+      }
+        
+      if (i > 0 && sortedArr[i] === sortedArr[i - 1]) {
+        continue;
+      }
+        
+      let start = i + 1;
+      let end = sortedArr.length - 1;
+      while (start < end) {
+        const sum = sortedArr[i] + sortedArr[start] + sortedArr[end];
+        if (sum < 0) {
+          start++;
+        } else if (sum > 0) {
+          end--;
+        } else {
+            
+          triplets.push([sortedArr[i], sortedArr[start], sortedArr[end]]);
+            
+          while (start < end && sortedArr[start] === sortedArr[end + 1]) {
+            start++;
+          }
+          while (start < end && sortedArr[end] === sortedArr[end - 1]) {
+            end--;
+          }
+          start++;
+          end--;  
+        }
+      }
+    }
+    return triplets;
+};
