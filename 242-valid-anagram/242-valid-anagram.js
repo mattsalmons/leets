51400a60ee44b3f22beeb8170cb0c1e82c412980
Matt Salmons <69879950+mattsalmons@users.noreply.github.com>
2022-01-20 07:47:36 -0700

    Time: 88 ms (85.38%), Space: 41.2 MB (58.54%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,36 +4,45 @@
  * @return {boolean}
  */
 
+// Writing your own helper function in a interview setting can score you a few extra points. Also, it's important to show your interviewer that you truly understand the problem by considering IOCE (Inputs, Outputs, Constraints, Edge cases). Even if you don't have time to write actual code, writing out each step in casual, conversational language could be enough. If you get to coding, you'll also have something to refer back to so you don't forget anything important. If I were asked this question in an interview, this is how I would respond:
+
+
 // input: two strings
-// output: boolean representing if two input strings are anagrams of eachother
-// constraints: O(n)
-// edge cases: spaces? special chars?
+// output: boolean representing whether two input strings are anagrams of eachother
+// constraints: O(n) time complexity (self imposed)
+// edge cases (ask interviewer about special cases not stated in problem):
+//    - empty inputs?
+//    - spaces?
+//    - special chars?
 
 // approach:
-//  - use object to count chars in each string (helper function)
-//  - compare both objects
-//      - if keys or values are different
-//          - return false
-//  - return true
-var isAnagram = function(s, t) {
-    if (s.length !== t.length) return false;
-    
-    const countChars = str => {
-        let charMap = {};
-        
-        for (let char of str) {
-            charMap[char] = charMap[char] + 1 || 1;
-        }
-        
-        return charMap;
-    }
-    
-    const sMap = countChars(s);
-    const tMap = countChars(t);
-    
-    for (let key in sMap) {
-        if (!tMap[key] || sMap[key] !== tMap[key]) return false;
-    }
-    
-    return true;    
-};
+//    - if length of inputs do not match, immediately return false
+//    - write helper function to count chars in each input (using counter objects/maps)
+//    - iterate over either counter object, compare to other
+//         - if keys or values are different return false
+//    - otherwise return true
+
+const isAnagram = (s, t) => {
+	if (s.length !== t.length) return false;
+	
+	// helper function
+	const countChars = string => {
+		const charMap = {};
+		
+		// using for...of syntax to help reduce chance of typos (instead of writing out traditional for loop)
+		for (let char of string) {
+			charMap[char] = charMap[char] + 1 || 1;
+		}
+		
+		return charMap;
+	}
+	
+	const sMap = countChars(s);
+	const tMap = countChars(t);
+	
+	for (let key in sMap) {
+		if (sMap[key] !== tMap[key]) return false;
+	}
+	
+	return true;
+}
