c7e3f117ab1560c83d6242f1198e9ad0cc2f66b8
Matt Salmons <69879950+mattsalmons@users.noreply.github.com>
2022-01-21 09:39:55 -0700

    Time: 64 ms (96.82%), Space: 38.9 MB (28.81%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    if (!digits.length) return [];
+    
+    const letterMap = {
+        2: ['a', 'b', 'c'],
+        3: ['d', 'e', 'f'],
+        4: ['g', 'h', 'i'],
+        5: ['j', 'k', 'l'],
+        6: ['m', 'n', 'o'],
+        7: ['p', 'q', 'r', 's'],
+        8: ['t', 'u', 'v'],
+        9: ['w', 'x', 'y', 'z']
+    }
+    
+    let result = [];
+    
+    function compile(string, index) {
+        if (index === digits.length) {
+            result.push(string);
+            return;
+        }
+        
+        for (let char of letterMap[digits[index]]) {
+            compile(string + char, index + 1);
+        }
+    }
+    
+    compile('', 0);
+    return result;
+};
