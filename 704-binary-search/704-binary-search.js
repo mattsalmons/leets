b5acadd1fbae07fffa8206ebd8da5cf3f09c1135
Matt Salmons <69879950+mattsalmons@users.noreply.github.com>
2022-01-24 07:30:23 -0700

    Time: 80 ms (72.80%), Space: 42.2 MB (58.89%) - LeetHub

---
+++ 704-binary-search/704-binary-search.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    
+    while (left <= right) {
+        let pivot = left + (right - left);
+        if (nums[pivot] === target) return pivot
+        if (target < nums[pivot]) right = pivot - 1;
+        else left = pivot + 1;
+    }
+    
+    return -1
+    
+};
