748d6c1175f6dc8f80561fc16b742b55cbd2866f
Matt Salmons <69879950+mattsalmons@users.noreply.github.com>
2022-01-26 20:36:53 -0700

    Time: 125 ms (20.97%), Space: 42.6 MB (13.38%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.js
+++ 63-unique-paths-ii/63-unique-paths-ii.js
@@ -6,10 +6,12 @@ var uniquePathsWithObstacles = function(obstacleGrid) {
     if (obstacleGrid[0][0] === 1) return 0;
     
     for (let i = 0; i < obstacleGrid.length; i++) {
-        for (let j = 0; j < obstacleGrid[i].length; j++) {
-            if (obstacleGrid[i][j] === 1) obstacleGrid[i][j] = 0;
+        let column = obstacleGrid[i];
+        for (let j = 0; j < column.length; j++) {
+            let row = column[j];
+            if (column[j] === 1) column[j] = 0;
             else if (i == 0 && j == 0) obstacleGrid[0][0] = 1;
-            else obstacleGrid[i][j] = (i > 0 ? obstacleGrid[i - 1][j] : 0) + (j > 0 ? obstacleGrid[i][j - 1] : 0)
+            else column[j] = (i > 0 ? obstacleGrid[i - 1][j] : 0) + (j > 0 ? obstacleGrid[i][j - 1] : 0)
         }
     }
     
