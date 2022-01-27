f0a70f0a6d68c3d503649fb83ff2b7b36c150356
Matt Salmons <69879950+mattsalmons@users.noreply.github.com>
2022-01-27 07:21:29 -0700

    Time: 81 ms (59.78%), Space: 45.3 MB (9.29%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -3,21 +3,47 @@
  * @param {number} target
  * @return {number}
  */
+// input:
+//  -sorted array of nums
+//  -target num
+// output:
+//  -index of target num in nums array
+//  -if not found, return -1
+// constraints:
+//  -use binary search
+// edge cases:
+//  -target is not a number return 'invalid target'
+// approach:
+//  -init left, mid, right
+//  -declare midFinder helper
+//  -iterate over nums array while left is less than right
+//      -if nums at mid index is target, return mid index
+//      -if nums at mid index is larger than target
+//          -reassign right and mid
+//              -otherwise
+//          -reassign left and mid
+//  -return -1 (only happens if target !== mid during loop)
+
 var search = function(nums, target) {
     let left = 0;
     let right = nums.length - 1;
-    let mid = Math.floor((left + right) / 2);
+    
+    function midFinder(indexOne, indexTwo) {
+        return Math.floor((indexOne + indexTwo) / 2);
+    }
+    
+    let mid = midFinder(left, right);
     
     while (left <= right) {
-        if (target === nums[mid]) return mid;
-        if (target > nums[mid]) {
-            left = mid + 1;
-            mid = Math.floor((left + right) / 2);
-        } else {
+        if (nums[mid] === target) return mid;
+        if (nums[mid] > target) {
             right = mid - 1;
-            mid = Math.floor ((left + right) / 2);
+            mid = midFinder(left, right);
+        } else {
+            left = mid + 1;
+            mid = midFinder(left, right);
         }
     }
     
-    return -1;
+    return - 1;
 };
