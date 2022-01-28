0876398b54b33321b9fc6e5d4f3435a2518ce486
Matt Salmons <69879950+mattsalmons@users.noreply.github.com>
2022-01-28 07:24:21 -0700

    Time: 69 ms (79.57%), Space: 41.9 MB (5.95%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.js
+++ 278-first-bad-version/278-first-bad-version.js
@@ -18,16 +18,13 @@ var solution = function(isBadVersion) {
      * @return {integer} The first bad version
      */
     return function(n) {
-        
         let left = 1;
         let right = n;
         
         while (left < right) {
-            let mid = Math.floor((right + left) / 2);
+            let mid = Math.floor((left + right) / 2);
             if (isBadVersion(mid)) right = mid;
-            else {
-                left = mid + 1;
-            }
+            else left = mid + 1;
         }
         
         return left;
