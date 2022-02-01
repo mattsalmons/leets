c69dc134049ee21cd5025bdea3f5447490e197f3
Matt Salmons <69879950+mattsalmons@users.noreply.github.com>
2022-01-31 18:39:31 -0700

    Time: 74 ms (70.59%), Space: 42.1 MB (5.17%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -34,7 +34,7 @@ var searchInsert = function(nums, target) {
     while (left <= right) {
         let mid = Math.floor((left + right) / 2);
         if (target === nums[mid]) return mid;
-        else if (target < nums[mid]) right = mid - 1;
+        if (target < nums[mid]) right = mid - 1;
         else left = mid + 1;
     }
     
