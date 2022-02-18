050f51a1bbd9db92e70777b217fd812cddf75609
Matt Salmons <69879950+mattsalmons@users.noreply.github.com>
2022-02-18 07:33:44 -0700

    Time: 81 ms (66.77%), Space: 44.6 MB (34.92%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -25,17 +25,13 @@
 //  -return -1 (only happens if target !== mid during loop)
 
 var search = function(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
+    let [left, right] = [0, nums.length - 1];
     
     while (left <= right) {
-        let mid = Math.floor((right + left) / 2);
+        let mid = Math.floor((left + right) / 2);
         if (target === nums[mid]) return mid;
-        if (target < nums[mid]) {
-            right = mid - 1;
-        } else {
-            left = mid + 1;
-        }
+        if (target < nums[mid]) right = mid - 1;
+        else left = mid + 1;
     }
     
     return -1;
